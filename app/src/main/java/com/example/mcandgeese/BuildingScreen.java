86d406d55fec9dbbe4b6b04dbb42e1893f4668e5
Daivik Goel <daivikgoel98@gmail.com>
2021-06-30 15:30:15 -0400

    Initial Commit with Android Project File and Buttons to each component

---
+++ app/src/main/java/com/example/mcandgeese/BuildingScreen.java
@@ -0,0 +1,14 @@
+package com.example.mcandgeese;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuildingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_building_screen);
+    }
+}
---
+++ app/src/main/java/com/example/mcandgeese/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mcandgeese;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void goToMapScreen(View view) {
+        Intent intent = new Intent(MainActivity.this, MapScreen.class);
+        startActivity(intent);
+    }
+    public void goToBuildingScreen(View view) {
+        Intent intent = new Intent(MainActivity.this, BuildingScreen.class);
+        startActivity(intent);
+    }
+    public void goToBattleScreen(View view) {
+        Intent intent = new Intent(MainActivity.this, BattleScreen.class);
+        startActivity(intent);
+    }
+}
