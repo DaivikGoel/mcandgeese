0832d81bd2e3398c68c6cd51785fff5a542e1c2a
Lichen Ma <lichenma123@gmail.com>
2021-07-30 11:36:02 -0400

    fixed layout and added skeleton for persisting data

--- app/src/main/java/com/example/mcandgeese/MainActivity.java
+++ app/src/main/java/com/example/mcandgeese/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mcandgeese;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
@@ -10,12 +11,34 @@ import android.content.Intent;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String sharedPreferencesKey = "MCANDGEESEPREF";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        // Storing data into SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences(sharedPreferencesKey,MODE_PRIVATE);
+        SharedPreferences.Editor edit = sharedPreferences.edit();
+        GlobalVariables globalVariables = (GlobalVariables) this.getApplication();
+        edit.putInt("health", globalVariables.getCurrentHealth());
+        edit.commit();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        // Retrieving data from SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences(sharedPreferencesKey,MODE_PRIVATE);
+        GlobalVariables globalVariables = (GlobalVariables) this.getApplication();
+        globalVariables.setCurrentHealth(sharedPreferences.getInt("health",0));
+    }
+
     public void goToMapScreen(View view) {
         Intent intent = new Intent(MainActivity.this, transition_screen.class);
         intent.putExtra("BUILDING_ID", "begin");
