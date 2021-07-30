75053b685b70e49aae0938971bf7af07a75de52d
Ninad Sakhardande <ninad.sakhardande@gmail.com>
2021-07-30 15:37:27 +0530

    created new component and layout for scrollable map

---
+++ app/src/main/java/com/example/mcandgeese/ScrollableMap.java
@@ -0,0 +1,14 @@
+package com.example.mcandgeese;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScrollableMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollable_map);
+    }
+}
