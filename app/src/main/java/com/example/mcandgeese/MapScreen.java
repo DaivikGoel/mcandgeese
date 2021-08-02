7c6c585f524a6f19d45d5999d32b263830b53fcd
Daivik Goel <daivikgoel98@gmail.com>
2021-08-01 21:24:43 -0400

    Added Monster Icons to Map Screen

--- app/src/main/java/com/example/mcandgeese/MapScreen.java
+++ app/src/main/java/com/example/mcandgeese/MapScreen.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.HashMap;
 
 public class MapScreen extends AppCompatActivity {
     ImageView firstImage;
@@ -14,6 +17,41 @@ public class MapScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_screen);
         firstImage = (ImageView) findViewById(R.id.firstImage);
+        HashMap<Integer, Integer> buildingToMonster = ((GlobalVariables) this.getApplication()).getBuildingToMonster();
+
+        Button E5 = (Button) findViewById(R.id.E5);
+        Button E7 = (Button) findViewById(R.id.E7);
+        Button DP = (Button) findViewById(R.id.DP);
+        Button SLC = (Button) findViewById(R.id.SLC);
+        Button QNC = (Button) findViewById(R.id.QNC);
+
+        int unicode = 0x1F479;
+        String monster = "/" + getEmojiByUnicode(unicode);
+
+        if(buildingToMonster.get(1) != null){
+
+            E5.setText("E5" + monster);
+        }
+
+        if(buildingToMonster.get(2) != null){
+
+            E7.setText("E7" + monster);
+        }
+
+        if(buildingToMonster.get(3) != null){
+
+            SLC.setText("SLC" + monster);
+        }
+
+        if(buildingToMonster.get(4) != null){
+
+            DP.setText("DP" + monster);
+        }
+
+        if(buildingToMonster.get(5) != null){
+
+            QNC.setText("QNC" + monster);
+        }
 
 
         int imageResource = getResources().getIdentifier("@drawable/campusmap", null, this.getPackageName());
@@ -66,6 +104,9 @@ public class MapScreen extends AppCompatActivity {
         intent.putExtra("FINAL_EVENT", 1);
         startActivity(intent);
 
+    }
+    public String getEmojiByUnicode(int unicode){
+        return new String(Character.toChars(unicode));
     }
 
 }
